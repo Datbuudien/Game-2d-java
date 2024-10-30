cb5f41ed359ad0f3aae7b4eec062671a53bd4bea
Datbuudien <144156833+Datbuudien@users.noreply.github.com>
2024-10-30 17:00:40 +0700

    Update Entity.java

--- My2dGame/src/entity/Entity.java
+++ My2dGame/src/entity/Entity.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package entity;
 
 import java.awt.AlphaComposite;
@@ -9,7 +5,7 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
 
-//This SHIT stores variables of char , monster ,npcs
+//This  stores variables of char , monster ,npcs
 
 import java.awt.image.BufferedImage;
 import java.io.File;
